aa1880f5cd1bd16ce9c59585ce36dab37fe82a04
Carmelo Besid <carmelo.besiduw@gmail.com>
2021-09-24 00:35:53 +0800

    refactor: apple implementation - add react query implementation - refactor components to only affect data driven areas

--- src/pages/Apple.tsx
+++ src/pages/Apple.tsx
@@ -1,46 +1,64 @@
 import { Theme, makeStyles, createStyles } from "@material-ui/core/styles";
-import { Avatar, Box, ListItemAvatar, ListItemText, Container, Typography, List, ListItem } from "@material-ui/core";
-import { Link } from 'react-router-dom';
-import { DayIcon, AppleAvatar, AvocadoAvatar, CoconutAvatar, LemonAvatar, OrangeAvatar, PearAvatar } from "../assets/icons";
+import {
+  Avatar,
+  Box,
+  ListItemAvatar,
+  ListItemText,
+  Container,
+  Typography,
+  List,
+  ListItem,
+  CircularProgress,
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
+import {
+  DayIcon,
+  AppleAvatar,
+  AvocadoAvatar,
+  CoconutAvatar,
+  LemonAvatar,
+  OrangeAvatar,
+  PearAvatar,
+} from "../assets/icons";
 import { StyledButton } from "../custom/button/StyledButton";
-import { colors } from '../themes/variables';
-
+import { colors } from "../themes/variables";
+import { useContext, useEffect } from "react";
+import { SearchContext } from "../context/SearchContext";
+import useLogs from "../queries/useLogs";
 
 const getAvatar = (fruit: string) => {
-  switch(fruit) {
-    case 'apple':
-      return <AppleAvatar/>
-    case 'orange':
-      return <OrangeAvatar/>
-    case 'avocado':
-      return <AvocadoAvatar/>
-    case 'coconut':
-      return <CoconutAvatar/>
-    case 'lemon': 
-      return <LemonAvatar/>
-    case 'pear':
-      return <PearAvatar/>
+  switch (fruit) {
+    case "apple":
+      return <AppleAvatar />;
+    case "orange":
+      return <OrangeAvatar />;
+    case "avocado":
+      return <AvocadoAvatar />;
+    case "coconut":
+      return <CoconutAvatar />;
+    case "lemon":
+      return <LemonAvatar />;
+    case "pear":
+      return <PearAvatar />;
     default:
-      return <AppleAvatar/>
+      return <AppleAvatar />;
   }
-}
-
+};
 
 const useStyles = makeStyles((theme: Theme) => {
   return createStyles({
-    root: {
-
-    },
     infoContainer: {
-      display: 'flex',
-      justifyContent: 'space-between',
+      display: "flex",
+      justifyContent: "space-between",
     },
     listTitle: {
-      marginBottom: '30px',
+      marginBottom: "30px",
     },
     listContainer: {
       background: colors.background,
-      borderRadius: '5px',
+      flexGrow: 0,
+      borderRadius: "5px",
+      height: "350px",
       // width: '320px',
     },
     primaryText: {
@@ -51,125 +69,174 @@ const useStyles = makeStyles((theme: Theme) => {
       fontWeight: 400,
     },
     linkText: {
-      fontWeight: 700,
+      fontWeight: 400,
       color: theme.palette.primary.main,
     },
     setupButton: {
-      width: '250px',
-    }
-  })
-})
+      width: "250px",
+    },
+  });
+});
 
-const Apple = () => {
-
+const Greetings = () => {
   const classes = useStyles();
+  return (
+    <Container className={classes.infoContainer}>
+      <Box display="flex" alignItems="center">
+        <Typography variant="h5">Good day John!</Typography>
+      </Box>
+      <DayIcon />
+    </Container>
+  );
+};
 
-  const activities = [
-    {
-      name: 'Updated IVSI Form',
-      time: '5 minutes ago'
-    },
-    {
-      name: 'Added new Address Binding',
-      time: '1 hour ago'
-    },
-    {
-      name: 'Application Started',
-      time: '2 hours ago'
-    }
-  ];
-
-  const announcements = [
-    {
-      name: 'John Doe',
-      avatar: 'lemon',
-      activity: 'Created Address Binding for DAL'
-    },
-    {
-      name: 'Jane Doe',
-      avatar: 'orange',
-      activity: 'Uploaded new T- Kun data'
-    },
-    {
-      name: 'Jason Farmer',
-      avatar: 'coconut',
-      activity: 'Jason recently joined the team, hooray!'
-    },    
-    {
-      name: 'Promdi Boy',
-      avatar: 'pear',
-      activity: 'Created Strawberry'
-    },    
-  ]
+const ActivitiesAndAnnouncements = () => {
+  const { isLoading, data } = useLogs();
+  const classes = useStyles();
 
   return (
     <>
       <Container className={classes.infoContainer}>
-        <Box display='flex' alignItems="center">
-          <Typography variant="h5">Good day John!</Typography>
-        </Box>
-        <DayIcon/>
-      </Container >
-      <Container className={classes.infoContainer}>
         <Container>
-          <Typography variant="h6" className={classes.listTitle}>My Recent Activities</Typography>
-          <List className={classes.listContainer}>
-            {
-              activities.map((activity) => {
-                return (
-                  <ListItem key={activity.name}>
-                    <ListItemText classes= {{secondary: classes.secondaryText, primary: classes.primaryText}}  primary={activity.name} secondary={activity.time}/>
-                  </ListItem>
-                )
-              })
-            }
-            <Link to="/">
-              <ListItem>
-                <ListItemText primary="See more" className={classes.linkText}/>
-              </ListItem>
-            </Link>
-          </List>
-          <Box marginY={3}>
-            <Link to="/ivsi">
-              <StyledButton  variant="outlined" color="primary" className={classes.setupButton}>IVSI Form Setup</StyledButton>
-            </Link>
-          </Box>
-          <Link to="/bindings">
-          <StyledButton variant="outlined" color="primary" className={classes.setupButton}>Binding Setup</StyledButton>
-          </Link>
-        </Container>
-        <Container component="div">
+          <Typography variant="h6" className={classes.listTitle}>
+            My Recent Activities
+          </Typography>
         </Container>
         <Container>
-          <Typography variant="h6" className={classes.listTitle}>Announcement Board</Typography>
-          <List className={classes.listContainer}>
-            {
-              announcements.map((announcement) => {
-                return (
-                  <ListItem key={announcement.activity}>
-                    <ListItemAvatar>
-                      <Avatar>
-                        {getAvatar(announcement.avatar)}
-                      </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText classes= {{ primary: classes.primaryText}}  primary={announcement.name} secondary={announcement.activity}/>
-                  </ListItem>
-                )
-              })
-            }
-            <Link to="/">
-              <ListItem>
-                <ListItemText primary="See more" className={classes.linkText}/>
-              </ListItem>
-            </Link>
-          </List>
-          <Box marginY={3}>
-           <StyledButton variant="contained" color="primary">Go to Pattern Creation</StyledButton>
-          </Box>
+          <Typography variant="h6" className={classes.listTitle}>
+            Announcement Board
+          </Typography>
         </Container>
       </Container>
+      <Container className={classes.infoContainer}>
+        {isLoading ? (
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            height="350px"
+            width="100%"
+            overflow="none"
+          >
+            <CircularProgress />
+          </Box>
+        ) : (
+          <>
+            <Container>
+              <List className={classes.listContainer}>
+                {data.data.activities.map((activity: any) => {
+                  return (
+                    <ListItem key={activity.name}>
+                      <ListItemText
+                        classes={{
+                          secondary: classes.secondaryText,
+                          primary: classes.primaryText,
+                        }}
+                        primary={activity.name}
+                        secondary={activity.time}
+                      />
+                    </ListItem>
+                  );
+                })}
+                <Link to="/">
+                    <ListItem>
+                      <ListItemText
+                        primary="See more"
+                        className={classes.linkText}
+                      />
+                    </ListItem>
+                  </Link>
+              </List>
+            </Container>
+            <Container>
+              <List className={classes.listContainer}>
+                {data.data.announcements.map((announcement: any) => {
+                  return (
+                    <ListItem key={announcement.activity}>
+                      <ListItemAvatar>
+                        <Avatar>{getAvatar(announcement.avatar)}</Avatar>
+                      </ListItemAvatar>
+                      <ListItemText
+                        classes={{ primary: classes.primaryText }}
+                        primary={announcement.name}
+                        secondary={announcement.activity}
+                      />
+                    </ListItem>
+                  );
+                })}
+                <Link to="/">
+                  <ListItem>
+                    <ListItemText
+                      primary="See more"
+                      className={classes.linkText}
+                    />
+                  </ListItem>
+                </Link>
+              </List>
+            </Container>
+          </>
+        )}
+      </Container>
     </>
-  )
-}
+  );
+};
+
+const SetupLinks = () => {
+  const classes = useStyles();
+
+  return (
+    <Container className={classes.infoContainer}>
+      <Container>
+        <Box marginY={3}>
+          <Link to="/ivsi">
+            <StyledButton
+              variant="outlined"
+              color="primary"
+              className={classes.setupButton}
+            >
+              IVSI Form Setup
+            </StyledButton>
+          </Link>
+        </Box>
+        <Link to="/bindings">
+          <StyledButton
+            variant="outlined"
+            color="primary"
+            className={classes.setupButton}
+          >
+            Binding Setup
+          </StyledButton>
+        </Link>
+      </Container>
+      <Container>
+        <Box marginY={3}>
+          <StyledButton variant="contained" color="primary">
+            Go to Pattern Creation
+          </StyledButton>
+        </Box>
+      </Container>
+    </Container>
+  );
+};
+
+const Apple = () => {
+  const { setSearch, setSearchIsHidden, setSearchPlaceholder } =
+    useContext(SearchContext);
+
+  useEffect(() => {
+    setSearchPlaceholder("Search T-kun...");
+    setSearch("");
+    setSearchIsHidden(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      <Greetings />
+      <ActivitiesAndAnnouncements />
+      <SetupLinks />
+    </>
+  );
+};
 
 export default Apple;
