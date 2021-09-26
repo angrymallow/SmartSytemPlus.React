4a4e6ef13adb00b8ef17b4a30127501ee02c642f
Carmelo Besid <carmelo.besiduw@gmail.com>
2021-09-26 10:30:36 +0800

    update global theming

--- src/themes/theme.tsx
+++ src/themes/theme.tsx
@@ -1,24 +1,25 @@
-import { createTheme } from '@material-ui/core/styles';
-import CentraRegular from '../assets/fonts/Centra-No2-Regular.woff2';
+import { createTheme } from "@material-ui/core/styles";
+import CentraRegular from "../assets/fonts/Centra-No2-Regular.woff2";
+import { colors } from "./variables";
 
 const centra = {
-  fontFamily: 'Centra No2',
-  fontStyle: 'normal',
+  fontFamily: "Centra No2",
+  fontStyle: "normal",
   fontWeight: 400,
   src: `
     url(${CentraRegular}) format('woff2')
   `,
   unicodeRange:
-    'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
+    "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF",
 };
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#0A8080',
+      main: colors.primary,
     },
     secondary: {
-      main: '#F45D48',
+      main: colors.secondary,
     },
   },
   shadows: [
@@ -48,21 +49,34 @@ const theme = createTheme({
     "0px 3px 28px -10px rgba(112,144,176, 0.2)",
     "0px 3px 28px -10px rgba(112,144,176, 0.2)",
   ],
-   typography: {
-    fontFamily: [
-      'Centra No2',
-    ].join(','),
+  typography: {
+    fontFamily: ["Centra No2"].join(","),
     body2: {
       fontWeight: 700,
     },
-    
   },
   overrides: {
     MuiCssBaseline: {
-      '@global': {
-        '@font-face': [centra],
+      "@global": {
+        "@font-face": [centra],
       },
     },
+    MuiMenuItem: {
+      root: {
+        "&:hover": {
+          backgroundColor: colors.primaryLight,
+          color: colors.primary,
+        },
+      },
+    },
+    MuiAvatar: {
+      root: {
+        backgroundColor: colors.primary,
+      },
+      colorDefault: {
+        backgroundColor: colors.primaryLight,
+      },
+    }
   },
 });
 
--- src/themes/variables.tsx
+++ src/themes/variables.tsx
@@ -2,4 +2,7 @@ export const colors = {
   background: '#FAFAFA',
   black5: '#F2F2F2',
   black36: '#A3A3A3',
+  primary: '#0A8080',
+  primaryLight: '#F3FAFB',
+  secondary: '#F45D48',
 }
