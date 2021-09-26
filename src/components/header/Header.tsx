896373142543b987061618a769644ba3bfdb4a26
Carmelo Besid <carmelo.besiduw@gmail.com>
2021-09-26 10:28:31 +0800

    feat: move out User Info component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,29 +1,24 @@
 import { useContext, useEffect, useState } from 'react';
-import { AppBar, Toolbar, InputBase, Typography, Menu, MenuItem, Divider } from '@material-ui/core';
-import { SearchOutlined as SearchIcon, KeyboardArrowDownOutlined as KeyboardArrowDownIcon } from '@material-ui/icons';
+import { AppBar, Toolbar, InputBase } from '@material-ui/core';
+import { SearchOutlined as SearchIcon } from '@material-ui/icons';
 import useStyles  from "./header.styles"
 import { AppLogo } from '../../assets/icons';
 import { SearchContext } from '../../context/SearchContext';
+import { UserInfo } from '../userinfo/UserInfo';
 
 export default function Header() {
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  
   const [keyword, setKeyword] = useState('');
   const {search, setSearch, isSearchHidden, searchPlaceholder: placeholder} = useContext(SearchContext);
-  const open = Boolean(anchorEl);
+  
 
   useEffect(() => {
     console.log(search, 'search value in effects')
     setKeyword(search)
   }, [search])
 
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
 
   const handleSearch = (e: any) => {
     if (e.key === 'Enter') {
@@ -56,36 +51,7 @@ export default function Header() {
             
             : null
         }
-        <div className={classes.userInfo} onClick={handleClick}>
-          <div>
-            <Typography variant="body1">Carmelo Besid</Typography>
-            <Typography variant="body2">Administrator</Typography>
-          </div>
-          <KeyboardArrowDownIcon className={classes.menuIcon}/>
-        </div>
-        <Menu
-          id="long-menu"
-          anchorEl={anchorEl}
-          keepMounted
-          open={open}
-          onClose={handleClose}
-          PaperProps={{
-            style: {
-              width: '20ch',
-            },
-          }}
-        >   
-        <MenuItem onClick={handleClose}>
-          Menu 1
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          Update Info
-        </MenuItem>
-        <Divider/>
-        <MenuItem onClick={handleClose}>
-          Logout
-          </MenuItem>
-        </Menu>
+        <UserInfo/>
       </Toolbar>
     </AppBar>
   )
