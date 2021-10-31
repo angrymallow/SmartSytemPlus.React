cdf374481d89efaf024883d949484cd366779457
Carmelo Besid <carmelo.besiduw@gmail.com>
2021-11-01 02:11:40 +0800

    feat: add use headings hooks

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,5 +1,6 @@
 import { useSheetNameValidation } from "./useSheetNameValidation";
 import { useBindingSetValueValidation } from "./useBindingSetValueValidation";
 import { useSearchKeywordValidation } from "./useSearchKeywordValidation";
+import { useHeadings } from "./useHeadings";
 
-export { useBindingSetValueValidation, useSheetNameValidation, useSearchKeywordValidation };
+export { useBindingSetValueValidation, useSheetNameValidation, useSearchKeywordValidation, useHeadings };
